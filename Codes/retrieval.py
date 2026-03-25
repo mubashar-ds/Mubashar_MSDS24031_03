d3c55fae62885b7c287cf71bf5f4e61dff8a9f8a
Mubashar Hussain <mubashar.cui@gmail.com>
2026-03-25 08:49:31 +0500

    added retrieval visualization

--- Codes/retrieval.py
+++ Codes/retrieval.py
@@ -57,21 +57,66 @@ def plotting_tsne(embeddings, labels, title):
     plt.savefig(f'../Graphs/{title}_tsne.png')
     plt.close()
 
+# visualizing retreival...
+
+def showing_retrieval(query_index, embeddings, images, labels, class_names, k = 5):
+
+    index = query_index
+
+    dist = np.linalg.norm(embeddings - embeddings[index], axis = 1)
+    nearest_neighbor_index = np.argsort(dist)[1:k+1]
+
+    plt.figure(figsize =(10, 3))
+
+    # query..
+
+    plt.subplot(1, k+1, 1)
+    plt.imshow(images[index])
+    plt.title('Query')
+    plt.axis('off')
+
+    # neighbors...
+
+    for i, j in enumerate(nearest_neighbor_index):
+
+        plt.subplot(1, k+1, i+2)
+        plt.imshow(images[j])
+
+        color = 'green' if labels[j] == labels[index] else 'red'
+        plt.title(class_names[labels[j]], color = color)
+
+        plt.axis('off')
+
+    plt.savefig(f'../Graphs/retrieval_{index}.png')
+    plt.close()
+
 # runnign evaluation...
 
 def evaluating(prefix):
 
-    embeddings, labels = loading_data(prefix)
+    embeddings, images , labels = loading_data(prefix)
 
     print(f'\n{prefix} ---')
 
     r1 = recall_at_k(embeddings, labels, k = 1)
-    r5 = recall_at_k(embeddings, labels, k = 5)
-
     print(f'Recall_at_1: {r1:.3f}')
+
+    r5 = recall_at_k(embeddings, labels, k = 5)
     print(f'Recall_at_5: {r5:.3f}')
 
     plotting_tsne(embeddings, labels, prefix)
+
+    # for loading class names...
+
+    dataset = datasets.ImageFolder('../Dataset/caltech-101')
+    class_names = dataset.classes
+
+    # 10 query visualizations..
+
+    indexes = np.random.choice(len(embeddings), 10, replace = False)
+
+    for index in indexes:
+        showing_retrieval(index, embeddings, images, labels , class_names, k = 5)
 
 if __name__ == '__main__':
 
